68c9c5644cda1488311b536178b209e109d41b4e
Carlos Araujo <142362978+cadu-araujo@users.noreply.github.com>
2025-02-10 23:24:35 -0400

    Add files via upload

---
+++ buscamedbanco.sql
@@ -0,0 +1,16 @@
+create database buscamed;
+use buscamed;
+
+show tables;
+
+show create table venda;
+show create table usuario;
+show create table login;
+show create table farmacia;
+show create table remedio;
+
+select * from venda;
+select * from usuario;
+select * from login;
+select * from farmacia;
+select * from remedio;
